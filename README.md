9697e1519008425d8b18646376dd612b67986740
imtiazazad <imtiaz16-549@diu.edu.bd>
2022-08-15 21:39:34 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+### Features
+
+- Fully Responsive
+- Multi-Page Layout
+- Contact Form With EmailJs
+- React-Bootstrap
+- Edit Content From One Place
+
+
+
+
+ 
+
+
---
+++ src/content_option.js
@@ -0,0 +1,168 @@
+const logotext = "Imtiaz";
+const meta = {
+    title: "Imtiaz Azad",
+    description: "I’m Imtiaz Azad  _ Full stack devloper,currently pursuing BSC in CIS",
+};
+
+const introdata = {
+    title: "I’m Imtiaz Azad",
+    animated: {
+        first: "I love coding",
+        second: "I code responsive websites",
+        third: "I develop web applications",
+    },
+    description: "Professional web developer with experience working across mern-stack website development. I’ve built a few projects by myself for company and I’m looking for a role where I can grow and learn from other experienced team members.",
+    your_img_url: "https://images.unsplash.com/photo-1660569841130-21f6929e31e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=572&q=80",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Professional web developer with experience working across mern-stack website development. I’ve built a few projects by myself for company and I’m looking for a role where I can grow and learn from other experienced team members.",
+};
+const worktimeline = [{
+        jobtitle: "Frontend Developer",
+        where: "Codixel",
+        date: "2022",
+    },
+    {
+        jobtitle: "Enumerator",
+        where: "World Food Programme",
+        date: "2018-2020",
+    },
+    {
+        jobtitle: "Data Analyst",
+        where: "BRAC",
+        date: "2017-2017",
+    },
+];
+
+const skills = [{
+        name: "HTML, CSS, Bootstrap, Tailwind, Material UI",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 85,
+    },
+    {
+        name: "React, Redux",
+        value: 80,
+    },
+    {
+        name: "Node js, Express js",
+        value: 60,
+    },
+    {
+        name: "MongoDb",
+        value: 60,
+    },
+];
+
+const services = [{
+        title: "Frontend Web Application",
+        description: "Able to create professional and fully responsive front-end web application, stunning, and user-friendly website from scratch.",
+    },
+    {
+        title: "Fix Web Bugs",
+        description: "I have developed several web applications from scratch but at the same time, I manage and update existing projects also as I have debugging skills (DEV Tools)",
+    },
+    {
+        title: "Mern-stack Web Application",
+        description: "I am a MERN-stack web developer. I have more than 2+ years of experience in the company and have good knowledge of front-end and back-end techniques for building web applications that match client needs. Skilled about web design too.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "Warehouse management site, worked on both frontend and backend",
+        link: "https://warehouse-management-269c6.web.app/",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "E-commerce website with React",
+        link: "https://embossed-e-commerce.web.app/",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "Travel site worked with mernstack.",
+        link: "https://dorext-88fcc.web.app/home",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "Dental site to visit doctor.",
+        link: "https://dental-clinic-1a8c1.web.app/hom",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "Trending, Best Selling Pottery And handmade Products.",
+        link: "https://qutir-mahal.web.app/",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "Tunicables media require login.",
+        link: "https://tunicalabs-media.web.app/form/signin",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "Car Accessories website with mernstack.",
+        link: "https://car-accessories-e4193.web.app/",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "Doctor visiting site",
+        link: "https://doctors-portal-db136.web.app/",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "Warehouse management full stack website",
+        link: "https://warehouse-f6a32.web.app/",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "Gym website for users.",
+        link: "https://gym-triner-9c7c3.web.app/",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "Food site with delivery",
+        link: "https://hardcore-pare-4a9874.netlify.app/",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "Electric website worked on frontend and backend",
+        link: "https://electric-tools-33155.web.app/",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "imtiaz.azad98@gmail.com",
+    YOUR_FONE: "+8801886180898",
+    description: "If you want a professional and fully responsive front-end web application, stunning, and user-friendly website from scratch, please contact me.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/imtiazazad",
+    facebook: "https://facebook.com/imazix",
+    linkedin: "https://www.linkedin.com/in/imtiazazad/",
+    twitter: "https://twitter.com/imtiazazad5",
+    youtube: "https://www.youtube.com/channel/UCyb6W9uUGXyzV8CGBQcAvJw",
+    
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
